75166f160c9cbccdcd13284303767024ba6cd10b
lmpham1 <lmpham1@myseneca.ca>
2022-06-14 04:43:24 -0400

    added api.js createFragment.js

--- src/api.js
+++ src/api.js
@@ -24,3 +24,25 @@ export async function getUserFragments(user) {
     console.error('Unable to call GET /v1/fragment', { err });
   }
 }
+
+export async function createFragmentData(user, fragmentData){
+  console.log('Creating new fragment...', {fragmentData});
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      // Generate headers with the proper Authorization bearer token to pass
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        ...user.authorizationHeaders()
+      },
+      body: JSON.stringify(fragmentData)
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Got user fragments data', { data });
+  } catch (err) {
+    console.error('Unable to call POST /v1/fragments', { err });
+  }
+}
---
+++ src/createFragment.js
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { createFragmentData } from './api'
+import { getUser } from "./auth";
+import { nanoid } from 'nanoid'
+import { Button } from "react-bootstrap";
+
+export default function CreateFragment(){
+    const [value, setValue] = useState("");
+    const handleSubmit = async function() {
+        console.log("handled submission " + value);
+        const user = await getUser();
+        console.log(user);
+        createFragmentData(user, {
+            id: nanoid(),
+            ownerid: user.username,
+            size: value.length,
+            created: JSON.stringify(new Date()),
+            type: 'text/plain',
+            data: value
+        })
+    }
+    const handleChange = function(e){
+        setValue(e.target.value);
+    }
+    return(
+        <div>
+        <p>
+          Fragment Text:
+          <input type="text" value={value} onChange={handleChange} />
+        </p>
+        <Button className="btn btn-success" onClick={handleSubmit}>Submit</Button>
+        </div>
+    )
+}
