f56973871f9c30dcb30bac0dd5f8a2ffb1282c55
Minh Pham <est2000vn@gmail.com>
2022-08-16 16:19:13 -0400

    removed unused routing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import { Button } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import { getUserFragments } from './api';
@@ -20,13 +19,6 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
